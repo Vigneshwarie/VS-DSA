bf872894e66d5129978ee72b8920f7e35d14460c
Vigneswari Sambandam <31098937+Vigneshwarie@users.noreply.github.com>
2024-08-25 13:56:20 -0700

    Fibonacci Optimized

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -11,10 +11,10 @@ def fibonacci_recursion(n):
         return fibonacci_recursion(n-1) + fibonacci_recursion(n-2)
 
 
-print(f"Start time : {time.time()} seconds")
-print([fibonacci_recursion(n) for n in range(30)])
-print(f"End time : {time.time()} seconds")
-print("\n")
+# print("\n")
+# print(f"Start time : {time.time()} seconds")
+# print([fibonacci_recursion(n) for n in range(35)])
+# print(f"End time : {time.time()} seconds")
 
 
 def fibonacci_optimized(n, a1, a2):
@@ -24,6 +24,21 @@ def fibonacci_optimized(n, a1, a2):
         return fibonacci_optimized(n-1, a2, a1+a2)
 
 
+print("\n")
 print(f"Start time : {time.time()} seconds")
-print([fibonacci_optimized(30, 0, 1)])
+print([fibonacci_optimized(n, 0, 1) for n in range(30)])
+print(f"End time : {time.time()} seconds")
+
+
+def fibonacci_bottomup_dynamic(n):
+    a1, a2 = 0, 1
+    for _ in range(n):
+        a1, a2 = a2, a1+a2
+
+    return a1
+
+
+print("\n")
+print(f"Start time : {time.time()} seconds")
+print([fibonacci_bottomup_dynamic(n) for n in range(30)])
 print(f"End time : {time.time()} seconds")
